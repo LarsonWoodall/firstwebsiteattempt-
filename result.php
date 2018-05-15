b3335e52c3e43c25a0740970b13bb069368de8df
Keenan Woodall <keenanwoodall@gmail.com>
2018-05-15 00:35:20 -0500

    starting html and php nav bar (larson did this)

---
+++ result.php
@@ -0,0 +1,16 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+        $me = $_POST{'me'};
+        $project = $_POST{'project'};
+        $contact = $_POST{'contact'};
+
+        if(isset($me)){
+        print "<div class='me'>
+                    <h1 class='bigheader'>Hey, it's Larson.</h1>
+               </div>"           
+    ?>
+</body>
